47b75e3b0e346919f3efd4b5f767c4820154f260
Ruud de Klerk <ruud@codeveloped.nl>
2023-01-03 10:43:02 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="djpwr-app-settings",
+    version="0.0.1",
+    description="Django Power Application Settings",
+    license='MIT',
+    author="Ruud de Klerk",
+    author_email="ruud@codeveloped.nl",
+    url='https://github.com/djpwr/djpwr-app-settings',
+    packages=find_packages()
+)
