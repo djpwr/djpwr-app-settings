93a7e0fb1eecbf53219f8001d4c5165f8a071def
Wietze Helmantel <helmantel@w20e.com>
2023-05-25 14:23:58 +0200

    Update admin.py
    Django 4 compat

--- djpwr_app_settings/admin.py
+++ djpwr_app_settings/admin.py
@@ -56,11 +56,11 @@ class SettingGroupAdmin(admin.ModelAdmin):
         return self.changeform_view(request, form_url, extra_context)
 
     @csrf_protect_m
-    def changeform_view(self, request, form_url='', extra_context=None):
+    def changeform_view(self, request, object_id=None, form_url="", extra_context=None):
         with transaction.atomic(using=router.db_for_write(self.model)):
-            return self._changeform_view(request, form_url, extra_context)
+            return self._changeform_view(request, object_id, form_url, extra_context)
 
-    def _changeform_view(self, request, form_url, extra_context):
+    def _changeform_view(self, request, object_id, form_url, extra_context):
         model = self.model
         opts = model._meta
 
