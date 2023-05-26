dc41b09ac3f8db80ddefba19a3ba3a882c7a403a
Wietze Helmantel <helmantel@w20e.com>
2023-05-26 10:48:05 +0200

    Update apps.py
    Fixed bootstrapping error

--- djpwr_app_settings/apps.py
+++ djpwr_app_settings/apps.py
@@ -10,14 +10,18 @@ class AppConfig(BaseAppConfig):
     verbose_name = _("Application settings")
 
     def ready(self):
-        from .models import MODEL_LABELS
+        try:
+            from .models import MODEL_LABELS
 
-        # Generate SettingGroups and ApplicationSettings for all registered models
-        for model_label in MODEL_LABELS:
-            model_class = get_model(model_label)
+            # Generate SettingGroups and ApplicationSettings for all registered models
+            for model_label in MODEL_LABELS:
+                model_class = get_model(model_label)
 
-            group_name = model_class.name_from_class()
+                group_name = model_class.name_from_class()
 
-            setting_group = get_manager('app_settings.SettingGroup').create_group(group_name)
+                setting_group = get_manager('app_settings.SettingGroup').create_group(group_name)
 
-            get_manager('app_settings.ApplicationSetting').create_for_group(setting_group)
+                get_manager('app_settings.ApplicationSetting').create_for_group(setting_group)
+        except:
+            # Bootstrapping: Migrations need to be run before the above will work.
+            pass
