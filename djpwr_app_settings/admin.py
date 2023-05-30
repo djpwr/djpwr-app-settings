018aa23ca0b058b3e2dc7951cbaf46e9972fb17c
Wietze Helmantel <helmantel@pythonunited.com>
2023-05-30 13:42:37 +0200

    Cleanup

--- djpwr_app_settings/admin.py
+++ djpwr_app_settings/admin.py
@@ -13,27 +13,22 @@ try:
     from django.utils.translation import gettext_lazy as _
 except:
     from django.utils.translation import ugettext_lazy as _
-    
+
 from djpwr.managers import get_manager
 
 from . import models, APP_SETTINGS
 
 
-class SettingGroupAdmin(admin.ModelAdmin):
-    list_display = ['group_name', 'last_modified']
-
-admin.site.register(models.SettingGroup, SettingGroupAdmin)
-
-
 class ApplicationSettingAdmin(admin.ModelAdmin):
-    list_display = ['group_name', 'name']
+    list_display = ["group_name", "name"]
+
 
 admin.site.register(models.ApplicationSetting, ApplicationSettingAdmin)
 
 
 class SettingGroupAdmin(admin.ModelAdmin):
     readonly_fields = ['last_modified']
-    exclude = ['group_name']
+    exclude = ["group_name"]
 
     def get_urls(self):
         from django.urls import path
@@ -41,18 +36,19 @@ class SettingGroupAdmin(admin.ModelAdmin):
         def wrap(view):
             def wrapper(*args, **kwargs):
                 return self.admin_site.admin_view(view)(*args, **kwargs)
+
             wrapper.model_admin = self
             return update_wrapper(wrapper, view)
 
         info = self.model._meta.app_label, self.model._meta.model_name
 
         urlpatterns = [
-            path('', wrap(self.change_view), name='%s_%s_change' % info),
-            path('', wrap(self.change_view), name='%s_%s_changelist' % info),
+            path("", wrap(self.change_view), name="%s_%s_change" % info),
+            path("", wrap(self.change_view), name="%s_%s_changelist" % info),
         ]
         return urlpatterns
 
-    def change_view(self, request, form_url='', extra_context=None):
+    def change_view(self, request, form_url="", extra_context=None):
         return self.changeform_view(request, form_url, extra_context)
 
     @csrf_protect_m
@@ -64,17 +60,17 @@ class SettingGroupAdmin(admin.ModelAdmin):
         model = self.model
         opts = model._meta
 
-        setting_group = get_manager('app_settings.SettingGroup').get(
+        setting_group = get_manager("app_settings.SettingGroup").get(
             group_name=model.name_from_class()
         )
 
         obj = model(
             pk=setting_group.pk,
             group_name=setting_group.group_name,
-            last_modified=setting_group.last_modified
+            last_modified=setting_group.last_modified,
         )
 
-        if request.method == 'POST':
+        if request.method == "POST":
             if not self.has_change_permission(request, obj):
                 raise PermissionDenied
         else:
@@ -85,19 +81,22 @@ class SettingGroupAdmin(admin.ModelAdmin):
 
         ModelForm = self.get_form(request, obj, change=not add)
 
-        if request.method == 'POST':
+        if request.method == "POST":
             form = ModelForm(request.POST, request.FILES, instance=obj)
             form_validated = form.is_valid()
             if form_validated:
                 new_object = self.save_form(request, form, change=not add)
             else:
                 new_object = form.instance
-            formsets, inline_instances = self._create_formsets(request, new_object,
-                                                               change=not add)
+            formsets, inline_instances = self._create_formsets(
+                request, new_object, change=not add
+            )
             if all_valid(formsets) and form_validated:
                 self.save_model(request, new_object, form, not add)
                 self.save_related(request, form, formsets, not add)
-                change_message = self.construct_change_message(request, form, formsets, add)
+                change_message = self.construct_change_message(
+                    request, form, formsets, add
+                )
                 if add:
                     self.log_addition(request, new_object, change_message)
                     return self.response_add(request, new_object)
@@ -108,10 +107,11 @@ class SettingGroupAdmin(admin.ModelAdmin):
                 form_validated = False
         else:
             initial = self.get_changeform_initial_data(request)
-            initial['last_modified'] = obj.last_modified
+            initial["last_modified"] = obj.last_modified
             form = ModelForm(initial=initial)
             formsets, inline_instances = self._create_formsets(
-                request, form.instance, change=False)
+                request, form.instance, change=False
+            )
 
         if not add and not self.has_change_permission(request, obj):
             readonly_fields = flatten_fieldsets(self.get_fieldsets(request, obj))
@@ -121,39 +121,47 @@ class SettingGroupAdmin(admin.ModelAdmin):
             form,
             list(self.get_fieldsets(request, obj)),
             # Clear prepopulated fields on a view-only form to avoid a crash.
-            self.get_prepopulated_fields(request, obj) if add or self.has_change_permission(
-                request, obj) else {},
+            self.get_prepopulated_fields(request, obj)
+            if add or self.has_change_permission(request, obj)
+            else {},
             readonly_fields,
-            model_admin=self)
+            model_admin=self,
+        )
         media = self.media + adminForm.media
 
-        inline_formsets = self.get_inline_formsets(request, formsets, inline_instances, obj)
+        inline_formsets = self.get_inline_formsets(
+            request, formsets, inline_instances, obj
+        )
         for inline_formset in inline_formsets:
             media = media + inline_formset.media
 
         if add:
-            title = _('Add %s')
+            title = _("Add %s")
         elif self.has_change_permission(request, obj):
-            title = _('Change %s')
+            title = _("Change %s")
         else:
-            title = _('View %s')
+            title = _("View %s")
         context = {
             **self.admin_site.each_context(request),
-            'title': title % opts.verbose_name,
-            'adminform': adminForm,
-            'original': obj,
-            'is_popup': IS_POPUP_VAR in request.POST or IS_POPUP_VAR in request.GET,
-            'media': media,
-            'inline_admin_formsets': inline_formsets,
-            'errors': helpers.AdminErrorList(form, formsets),
-            'preserved_filters': self.get_preserved_filters(request),
+            "title": title % opts.verbose_name,
+            "adminform": adminForm,
+            "original": obj,
+            "is_popup": IS_POPUP_VAR in request.POST or IS_POPUP_VAR in request.GET,
+            "media": media,
+            "inline_admin_formsets": inline_formsets,
+            "errors": helpers.AdminErrorList(form, formsets),
+            "preserved_filters": self.get_preserved_filters(request),
         }
 
         # Hide the "Save" and "Save and continue" buttons if "Save as New" was
         # previously chosen to prevent the interface from getting confusing.
-        if request.method == 'POST' and not form_validated and "_saveasnew" in request.POST:
-            context['show_save'] = False
-            context['show_save_and_continue'] = False
+        if (
+            request.method == "POST"
+            and not form_validated
+            and "_saveasnew" in request.POST
+        ):
+            context["show_save"] = False
+            context["show_save_and_continue"] = False
             # Use the change template instead of the add template.
             add = False
 
@@ -164,9 +172,11 @@ class SettingGroupAdmin(admin.ModelAdmin):
         )
 
     def get_changeform_initial_data(self, request):
-        settings = get_manager('app_settings.ApplicationSetting').filter(
-            group__group_name=self.model.name_from_class()
-        ).values_list('name', 'value')
+        settings = (
+            get_manager("app_settings.ApplicationSetting")
+            .filter(group__group_name=self.model.name_from_class())
+            .values_list("name", "value")
+        )
 
         return dict(settings)
 
@@ -177,9 +187,12 @@ class SettingGroupAdmin(admin.ModelAdmin):
             if field_name in obj._internal_fields:
                 continue
 
-            APP_SETTINGS['.'.join([group_name, field_name])] = value
+            APP_SETTINGS[".".join([group_name, field_name])] = value
 
         return obj
 
     def has_add_permission(self, request):
         return False
+
+
+admin.site.register(models.SettingGroup, SettingGroupAdmin)
