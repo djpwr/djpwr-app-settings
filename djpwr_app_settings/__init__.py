c738ca41ffcd0af11054d4ed96f187e3403b0223
Kim Chee Leong <leong@turftorr.nl>
2024-02-08 10:38:11 +0100

    Fix error when no migrations have been run

--- djpwr_app_settings/__init__.py
+++ djpwr_app_settings/__init__.py
@@ -1,16 +1,33 @@
+import logging
+
+from django.db import ProgrammingError
 from djpwr.managers import get_model, get_manager
 
 default_app_config = 'djpwr.app_settings.apps.AppConfig'
+
+
+logger = logging.getLogger(__name__)
 
 
 class AppSettingDict:
     def __getitem__(self, setting_label):
         app_label, model_name, setting_name = setting_label.split('.')
 
-        setting = get_manager('app_settings.ApplicationSetting').get(
-            group__group_name='.'.join([app_label, model_name]),
-            name=setting_name
-        )
+        try:
+            setting = get_manager('app_settings.ApplicationSetting').get(
+                group__group_name='.'.join([app_label, model_name]),
+                name=setting_name
+            )
+        except ProgrammingError:
+            # This exception occurs when the migrations of this app have not been
+            # run yet, as can happen when this app is newly added and this
+            # implementation is called from the code at Python runtime.
+            logger.error(
+                f"Catching exception in AppSettingDict.__getitem__ for {setting_label}, "
+                f" please check if migrations for this app have been run."
+
+            )
+            return
 
         return setting.value
 
